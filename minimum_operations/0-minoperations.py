a37d3c51e7bc971e1f749ccfce495b43daea4acc
Lalisa <l.iriza@alustudent.com>
2025-05-12 15:51:55 +0200

    changes made

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Module for calculating minimum number of operations to reach n H characters."""
+"""Module for calculating the minimum operations to reach n H characters."""
+
 
 def minOperations(n):
     """
-    Calculates the minimum number of operations needed to get exactly n H characters
-    using only Copy All and Paste operations.
+    Calculates the minimum number of operations needed to get exactly n H
+    characters using only Copy All and Paste operations.
 
     Args:
         n (int): Target number of characters.
@@ -25,20 +26,3 @@ def minOperations(n):
         factor += 1
 
     return operations
-#!/usr/bin/python3
-
-def minOperations(n):
-    if n < 2:
-        return 0
-
-    operations = 0
-    factor = 2
-
-    while n > 1:
-        while n % factor == 0:
-            operations += factor
-            n = n // factor
-        factor += 1
-
-    return operations
-
